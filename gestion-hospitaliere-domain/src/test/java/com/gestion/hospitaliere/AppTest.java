b54f9a4fa43b41ddb9509751f409ce7d595181f3
Esthaine <101167745+Esthaine@users.noreply.github.com>
2023-11-17 20:34:18 +0200

    Iniatialisation du projet

---
+++ gestion-hospitaliere-domain/src/test/java/com/gestion/hospitaliere/AppTest.java
@@ -0,0 +1,8 @@
+package com.gestion.hospitaliere;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+}
---
+++ gestion-hospitaliere-ws/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World222222</h2>
+<a href="/home">Go to home</a>
+</body>
+</html>
