49c5d81d47d80e64f9ac80fcc6f940107f915984
Esthaine <101167745+Esthaine@users.noreply.github.com>
2023-11-19 22:01:54 +0200

    Doctor Repository ajouter

---
+++ gestion-hospitaliere-domain/src/main/java/com/gestion/hospitaliere/config/DaoConfig.java
@@ -0,0 +1,4 @@
+package com.gestion.hospitaliere.config;
+
+public class DaoConfig {
+}
--- gestion-hospitaliere-domain/src/main/java/com/gestion/hospitaliere/entity/Docteur.java
+++ gestion-hospitaliere-domain/src/main/java/com/gestion/hospitaliere/entity/Docteur.java
@@ -1,10 +1,10 @@
 package com.gestion.hospitaliere.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-import javax.persistence.Entity;
 
 @Entity
 public class Docteur {
@@ -12,4 +12,5 @@ public class Docteur {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
 }
