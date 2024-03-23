035f4b87c7b6a43d937f5566806273262f008880
Esthaine <101167745+Esthaine@users.noreply.github.com>
2024-03-23 15:44:36 +0200

    Jakarta Role Privilege

--- gestion-hospitaliere-persistence/src/main/java/com/gestion/hospitaliere/entity/Privilege.java
+++ gestion-hospitaliere-persistence/src/main/java/com/gestion/hospitaliere/entity/Privilege.java
@@ -1,7 +1,7 @@
 package com.gestion.hospitaliere.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Privilege extends AbstractEntity{
